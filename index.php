030ea753a57906ecc2ace90bf49f1cbab4806b11
BiaBaccaro <130668481+BiaBaccaro@users.noreply.github.com>
2023-07-28 08:04:52 -0300

    commit da bia

--- index.php
+++ index.php
@@ -1,4 +1,14 @@
-
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>JOKENPO</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+    <script src='main.js'></script>
+</head>
+<body>
 
 <?php
 function jogarPedraPapelTesoura($escolhaUsuario) {
@@ -12,8 +22,8 @@ function jogarPedraPapelTesoura($escolhaUsuario) {
     if ($escolhaUsuario === $escolhaMaquina) {
         return "Empate. Você escolheu $escolhaUsuario e a máquina escolheu $escolhaMaquina.";
     } elseif (
-        ($escolhaUsuario === 'pedra' && $escolhaMaquina === 'tesoura') ||
-        ($escolhaUsuario === 'papel' && $escolhaMaquina === 'pedra') ||
+        ($escolhaUsuario === 'pedra' && $escolhaMaquina === 'tesoura')
+        ($escolhaUsuario === 'papel' && $escolhaMaquina === 'pedra')
         ($escolhaUsuario === 'tesoura' && $escolhaMaquina === 'papel')
     ) {
         return "Você venceu. Você escolheu $escolhaUsuario e a máquina escolheu $escolhaMaquina.";
@@ -23,3 +33,6 @@ function jogarPedraPapelTesoura($escolhaUsuario) {
 }
 
 ?>
+
+</body>
+</html>
